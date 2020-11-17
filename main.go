addf0f35259c25b657600b2e0b5d8cfc97e66dbf
Simon J Mudd <sjmudd@pobox.com>
2020-11-17 23:02:36 +0100

    Test script to validate interpolation behaviour

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	size := 100 * 1000
+	dsn := "root:root@tcp(127.0.0.1:3306)/"
+
+	if len(os.Args) > 1 {
+		if os.Args[1] == "interpolate" {
+			log.Print("setting interpolateParams=true")
+			dsn += "?interpolateParams=true"
+		}
+	}
+
+	log.Print("dsn:", dsn)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Open failed: %v", err)
+	}
+	log.Print("Open succeeded")
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Ping failed: %v", err)
+	}
+
+	// do a simple query to check things are workingg
+	sql := "SELECT 1"
+	rows, err := db.Query(sql)
+	if err != nil {
+		log.Fatalf("simple query failed: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		if err := rows.Scan(&id); err != nil {
+			log.Fatalf("Scan failed: %v", err)
+		}
+		log.Printf("Got back: %v", id)
+	}
+
+	// build the very large list
+	if len(os.Args) > 2 {
+		size, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Fatalf("strconvAtoi of arg 2 failed: %v", err)
+		}
+		log.Printf("size set to %v", size)
+	}
+	fixed := []string{}
+	args := []interface{}{}
+	for i := 0; i < size; i++ {
+		fixed = append(fixed, "?")
+		args = append(args, 1)
+	}
+	sql = "SELECT " + strings.Join(fixed, ",")
+
+	_, err = db.Query(sql, args...)
+	if err != nil {
+		log.Fatalf("Query failed: err: %q, sql: %q, args: %+v", err, sql, args)
+	}
+	log.Printf("query worked with size: %v", size)
+}
